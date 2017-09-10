9c2f8eed6a65c1bace04bd3fa5d637c3eb8e5225
Felipe-Omar <felipeomar.c@gmail.com>
2017-09-10 19:27:08 -0300

    update_spatial_filter

---
+++ projects/spatial_filter/codes/CMakeFiles/spatial_filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spatial_filter.dir/spatial_filter.cpp.o"
+  "spatial_filter.pdb"
+  "spatial_filter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spatial_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
